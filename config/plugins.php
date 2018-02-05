2aff109a78d25a69dfce272c1d8d94a3ac1cf313
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-05 07:44:58 +0100

    Allow login if ad is down

--- config/plugins.php
+++ config/plugins.php
@@ -18,9 +18,8 @@ define('WEB_FONT', 'Roboto');
  * @var string
  */
  define('AD_INTEGRATION_URL', 'https://intranat.helsingborg.se/ad-api/');
-
-/**
- * Integration with ad (domain used for ad-accounts)
- * @var string
- */
+ define('AD_UPDATE_NAME', true);
+ define('AD_UPDATE_EMAIL', true);
+ define('AD_SAVE_PASSWORD', true);
+ define('AD_RANDOM_PASSWORD', false);
  define('AD_USER_DOMAIN', 'helsingborg.se');
