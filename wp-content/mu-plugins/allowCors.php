7dcd4d31e2b2b8769ef0f2d80533d368d5c61cd1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-28 14:53:51 +0200

    Removes headers that creates fetching issues with auth method

--- wp-content/mu-plugins/allowCors.php
+++ wp-content/mu-plugins/allowCors.php
@@ -19,10 +19,6 @@ class AllowCors
         remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
         add_filter('rest_pre_serve_request', function( $value ) {
             header('Access-Control-Allow-Origin: *');
-            header('Access-Control-Allow-Methods: GET, POST');
-            header('Access-Control-Allow-Credentials: true');
-            header('Access-Control-Expose-Headers: Link', false);
-            header('Access-Control-Allow-Headers: X-Requested-With');
             return $value;
         });
     }
