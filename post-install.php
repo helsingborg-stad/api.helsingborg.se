a010b8cdf2f14ba76853b16124f1f1044a7f125d
Thor Brink <thor.brink@helsingborg.se>
2025-09-25 10:36:53 +0000

    chore: remove must-use-loader dependency and related post-install script

--- post-install.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $muPluginsDir = __DIR__ . '/wp-content/mu-plugins/';
-    $moveTo = $muPluginsDir . 'must_use_loader.php';
-    $muLoaderPath = $muPluginsDir.'must-use-loader/must_use_loader.php';
-    
-    if(file_exists($muLoaderPath)) {
-        rename($muLoaderPath, $moveTo);
-    }
